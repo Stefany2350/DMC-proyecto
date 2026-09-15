b60ceba9298f413329cc463ff99e0b8c432a23d9
s.salazar <stefanysalazar93@gmail.com>
2026-09-14 21:53:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ st.sidebar.image("DMC.png")
 modulos = st.sidebar.selectbox("Selecione la sección a consultar",["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if modulos == "Home":
-  st.write("Nombre: Stefany Salazar Espinoza  
-Nombre del módulo : Python Fundamentals
-Año: 2026  
-El presente proyecto xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-Las tecnologías utilizadas para este proyecto fueron xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-
-
+  st.write("Nombre: Stefany Salazar Espinoza")
+  st.write("Nombre del módulo : Python Fundamentals")
+  st.write("Año: 2026")
+  st.write("El presente proyecto xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+  st.write("Las tecnologías utilizadas para este proyecto fueron xxxxxxxxxxxxxxxxxxxxxxxxxxx")
+  
 
 """if modulos == "Listas":
   st.write("Te encuentras en el módulo de listas")
