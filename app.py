627bb35cdfb05aea37cb6ebb21b1e8a205c6d494
s.salazar <stefanysalazar93@gmail.com>
2026-09-20 14:09:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ if modulos == "Home":
         st.write("**Estudiante:** Stefany Salazar Espinoza")
 
     with col_info3:
-        st.write("**Año:** 2026")
+        st.write("**          Año:** 2026")
 
     st.markdown("---")
 
